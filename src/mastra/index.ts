4612c6f138dc0858c59ee5555e4be024628d64d5
Yujohn Nattrass <Yujohn.natt@gmail.com>
2025-04-22 10:10:57 -0700

    try langfuse

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,8 +1,9 @@
 
 import { Mastra } from '@mastra/core/mastra';
-import { createLogger } from '@mastra/core/logger';
+import { createLogger, combineLoggers } from '@mastra/core/logger';
 import { weatherAgent } from './agents';
 import { UpstashTransport } from '@mastra/loggers/upstash';
+import { LangfuseExporter } from 'langfuse-vercel';
 const transporter = new UpstashTransport({
   upstashUrl: process.env.UPSTASH_URL!,
   upstashToken: process.env.UPSTASH_TOKEN!,
@@ -21,17 +22,18 @@ console.log(`~~~~~~~~~~~~~~~`)
 export const mastra = new Mastra({
   agents: { weatherAgent },
   logger,
-  // telemetry: {
-  //   serviceName: 'clickhouse-test',
-  //   enabled: true,
-  //   sampling: {
-  //     type: 'always_on'
-  //   },
-  //   export: {
-  //     type: 'custom',
-  //     exporter: new MastraCloudExporter({
-  //       accessToken: process.env.MASTRA_CLOUD_ACCESS_TOKEN!,
-  //     }),
-  //   },
-  // }
+  telemetry: {
+    serviceName: 'ai', // this must be set to "ai" so that the LangfuseExporter thinks it's an AI SDK trace
+    enabled: true,
+    export: {
+      type: 'custom',
+      exporter: new LangfuseExporter({
+        publicKey: process.env.LANGFUSE_INIT_PROJECT_PUBLIC_KEY,
+        secretKey: process.env.LANGFUSE_INIT_PROJECT_SECRET_KEY,
+        baseUrl: 'http://localhost:3000', // Local Langfuse server from docker-compose
+      }),
+    },
+  },
 });
+
+console.log(mastra.getLogger().transports)
