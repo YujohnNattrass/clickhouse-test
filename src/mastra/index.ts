5f6bbfe55a5773bce93f698393d4f8a96cbc2068
Yujohn Nattrass <Yujohn.natt@gmail.com>
2025-04-22 21:23:28 -0700

    add-middleware

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -3,6 +3,7 @@ import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 import { weatherAgent } from './agents';
 import { UpstashTransport } from '@mastra/loggers/upstash';
+import { registerApiRoute } from "@mastra/core/server";
 const transporter = new UpstashTransport({
   upstashUrl: process.env.UPSTASH_URL!,
   upstashToken: process.env.UPSTASH_TOKEN!,
@@ -13,13 +14,30 @@ const logger = createLogger({
   transports: { transporter },
 });
 
-console.log(`~~~~~~~~~~~~~~~`)
-console.log(`WHAT THE HELL IS ${process.env.UPSTASH_URL} AND ${process.env.UPSTASH_TOKEN}`)
-console.log(`~~~~~~~~~~~~~~~`)
-
-
 export const mastra = new Mastra({
   agents: { weatherAgent },
   logger,
+  server: {
+    middleware: [
+      {
+        path: "/api/*",
+        handler: async (c, next) => {
+          // const apiKey = c.req.header("Authorization");
+          const isFromMastraCloud = c.req.header("x-mastra-cloud") === "true";
+
+          if (!isFromMastraCloud) {
+            return new Response("Unauthorized", { status: 401 });
+          }
+
+          // if (apiKey) {
+          //   const runtimeContext = c.get("runtimeContext");
+          //   runtimeContext.set("api-key", apiKey);
+          // }
+
+          await next();
+        },
+      },
+    ],
+  },
 });
 
