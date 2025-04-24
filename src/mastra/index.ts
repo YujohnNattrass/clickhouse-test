9e8aa5573c18bb10beb140ef7fe914a68c9566e9
Yujohn Nattrass <Yujohn.natt@gmail.com>
2025-04-23 17:24:57 -0700

    default storage

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,42 +2,33 @@
 import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 import { weatherAgent } from './agents';
-import { UpstashTransport } from '@mastra/loggers/upstash';
+import { DefaultStorage } from '@mastra/core/storage/libsql';
 // import { registerApiRoute } from "@mastra/core/server";
-const transporter = new UpstashTransport({
-  upstashUrl: process.env.UPSTASH_URL!,
-  upstashToken: process.env.UPSTASH_TOKEN!,
-})
-const logger = createLogger({
-  name: 'Mastra',
-  level: 'info',
-  transports: { transporter },
+
+// async function initMastra() {
+//   const { UpstashTransport } = await import('@mastra/loggers/upstash');
+//   const transporter = new UpstashTransport({
+//     upstashUrl: process.env.UPSTASH_URL!,
+//     upstashToken: process.env.UPSTASH_TOKEN!,
+//   })
+  
+//   const logger = createLogger({
+//     name: 'Mastra',
+//     level: 'info',
+//     transports: { transporter },
+//   });
+// }
+const storage = new DefaultStorage({
+  config: {
+    url: process.env.LIBSQL_URL!,
+    authToken: process.env.LIBSQL_TOKEN!,
+  },
 });
 
 export const mastra = new Mastra({
   agents: { weatherAgent },
-  logger,
-  // server: {
-  //   middleware: [
-  //     {
-  //       path: "/api/*",
-  //       handler: async (c, next) => {
-  //         // const apiKey = c.req.header("Authorization");
-  //         const isFromMastraCloud = c.req.header("x-mastra-cloud") === "true";
-
-  //         if (!isFromMastraCloud) {
-  //           return new Response("Unauthorized", { status: 401 });
-  //         }
-
-  //         // if (apiKey) {
-  //         //   const runtimeContext = c.get("runtimeContext");
-  //         //   runtimeContext.set("api-key", apiKey);
-  //         // }
-
-  //         await next();
-  //       },
-  //     },
-  //   ],
-  // },
+  storage,
 });
+// const mastra = await initMastra();
+// export { mastra };
 
