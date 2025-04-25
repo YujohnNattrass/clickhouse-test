73685b643075e6b676cd74f34aa3edad94b207ac
Yujohn Nattrass <Yujohn.natt@gmail.com>
2025-04-25 16:37:18 -0700

    Vercel deployement

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,7 +1,24 @@
 
 import { Mastra } from '@mastra/core/mastra';
 import { weatherAgent } from './agents';
+import { MastraCloudExporter } from '@mastra/cloud';
+import { VercelDeployer } from '@mastra/deployer-vercel';
 
 export const mastra = new Mastra({
   agents: { weatherAgent },
+  telemetry: {
+    serviceName: 'from-vercel',
+    export: {
+      type: 'custom',
+      exporter: new MastraCloudExporter({
+        accessToken: process.env.MASTRA_CLOUD_ACCESS_TOKEN as string,
+        endpoint: process.env.MASTRA_CLOUD_ENDPOINT as string
+      })
+    }
+  },
+  deployer: new VercelDeployer({
+    teamSlug: 'yujohnnattrass',
+    projectName: 'clickhouse-test',
+    token: process.env.V_TOKEN as string
+  })
 });
