3ba9b84887460f86ad5486696f7f13f3d3027040
Yujohn Nattrass <Yujohn.natt@gmail.com>
2025-05-02 08:49:07 -0700

    remove import

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,7 +2,6 @@
 import { Mastra } from '@mastra/core/mastra';
 import { weatherAgent } from './agents';
 import { weatherWorkflow } from './workflow';
-import { MastraCloudExporter } from '@mastra/cloud';
 
 export const mastra = new Mastra({
   agents: { weatherAgent },
